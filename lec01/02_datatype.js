e5c950e26dab6fe48601c55147d3612daf4deba1
ASHISH JOSHI <75516234+aj112001@users.noreply.github.com>
2023-12-16 09:25:11 +0000

    datatpes

---
+++ lec01/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version 
+
+// alert(3+3) //  give error because we are using nodejs, not browser
+
+// code readability should be high 
+
+
+// let name = "hitesh"
+// let age = 18
+// let isLoggedIn = False 
+
+// number
+// bigint
+// string => ""
+// boolean => t/f
+// null => standalone value( ex temperature ) it is a object
+// undefined =>  
+// symbol 
+
+// object 
+
+console.log(typeof "hitesh")
