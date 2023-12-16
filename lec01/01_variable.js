c163a29b63322e53632c1d0ac57bd0071b830234
ASHISH JOSHI <75516234+aj112001@users.noreply.github.com>
2023-12-16 09:31:47 +0000

    basic

---
+++ lec01/01_variable.js
@@ -0,0 +1,15 @@
+const accountId =35467
+let accountEmail = "ashu@gmail.com"
+var accountPassword = "93773"
+accountCity ="delhi"
+let accountState; // undefined // 
+
+accountPassword ="674884"
+accountEmail = "ghdsnd@gmail.com"
+accountCity = "haldwani"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/* 
+ Use most of the time const and let . 
+ kindly prefer not to use (var) , because of block scope and functional scope 
+*/ 
