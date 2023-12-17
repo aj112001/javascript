99fb1c316c0e24f951b80978664cb462550c66c4
ASHISH JOSHI <75516234+aj112001@users.noreply.github.com>
2023-12-17 16:42:32 +0000

    conversion of datatypes

---
+++ lec01/03_datacont.js
@@ -0,0 +1,17 @@
+let score = "33"
+// console.log(typeof score)
+
+let convertTonum = Number(score)
+// console.log(typeof convertTonum);
+
+// "33" => 33
+// "33abc" => NaN
+
+let logIn = 1
+let blogIn = Boolean(logIn)
+console.log(typeof blogIn)
+console.log(blogIn)
+
+// 1 => true      0 => false
+
+// "" => false    "ashu" => true
